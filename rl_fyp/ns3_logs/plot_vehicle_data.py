d14c571066884ca7332d4e2e585faa43f86babac
jihad <jkhurfan.jk@gmail.com>
2020-03-01 23:34:35 +0200

    added reward plot and multiple duration plots

--- rl_fyp/ns3_logs/plot_vehicle_data.py
+++ rl_fyp/ns3_logs/plot_vehicle_data.py
@@ -4,14 +4,18 @@ import sys
 import statistics
 import matplotlib.pylab as plt
 
-infile = "rsu_0.log"
+rsu_data_file = "rsu_0.log"
+rewards_file = "rewards.log"
+
 speeds_avg = {}
 speeds_std = {}
 headways_avg = {}
 headways_std = {}
+rewards = {}
 
-with open(infile) as fp:
-    line = fp.readline()
+
+with open(rsu_data_file,'r') as f1,  open(rewards_file, 'r') as f2:
+    line = f1.readline()
     time = 0
     max_time = 9000
     speeds = []
@@ -19,20 +23,21 @@ with open(infile) as fp:
     try:
         while line:
             if "time" in line:
-
                 time = float(line[line.find("=")+1 : line.find(":")].strip())
-                
+                reward = float(f2.readline().split(':')[1].strip())
+                rewards[time] = reward
+
                 if (time>max_time):
                     break
 
-            line = fp.readline()
+            line = f1.readline()
 
             while "time" not in line:
                 vals = line.split("::")
                 if len(vals) > 1:
                     speeds.append(float(vals[1].strip()))
                     headways.append(float(vals[2].strip()))
-                line = fp.readline()
+                line = f1.readline()
 
             speeds_avg[time] = statistics.mean(speeds)
             speeds_std[time] = statistics.stdev(speeds)
@@ -47,27 +52,39 @@ with open(infile) as fp:
       
         lists = sorted(speeds_avg.items())
         t, avg = zip(*lists)
-        plt.plot(t, avg, 'g', label="Average")
+
+        fig = plt.figure(1)
+        fig.suptitle('Velocity', fontsize=16)
+        plt.plot(t, avg, 'b-', label="Average")
 
         lists = sorted(speeds_std.items())
         t, std = zip(*lists)
-        plt.plot(t, std,'r', label="Standard Deviation")
+        plt.plot(t, std,'r--', label="Standard Deviation")
 
-        # fig = plt.figure()
-        # fig.suptitle('Velocity', fontsize=16)
         plt.legend()
-        plt.show()
+        # plt.show()
+
+        fig = plt.figure(2)
+        fig.suptitle('Headways', fontsize=16)
 
         lists = sorted(headways_avg.items())
         t, avg = zip(*lists) 
-        plt.plot(t, avg, 'g', label="Average")
+        plt.plot(t, avg, 'b-', label="Average")
 
         lists = sorted(headways_std.items())
         t, std = zip(*lists)
-        plt.plot(t, std,'r', label="Standard Deviation")
-        # fig = plt.figure()
-        # fig.suptitle('Headways', fontsize=16)
+        plt.plot(t, std,'r--', label="Standard Deviation")
+       
         plt.legend()
+
+        fig = plt.figure(3)
+        fig.suptitle('Rewards', fontsize=16)
+
+        lists = sorted(rewards.items())
+        t, vals = zip(*lists) 
+        plt.plot(t, vals, 'g-', label="Reward")
+        plt.legend()
+
         plt.show()
 
 
