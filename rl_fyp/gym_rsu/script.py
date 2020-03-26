1675b1c1870986fe494ad30bf0ccb7a0f9a59267
alt113 <rrn13@mail.aub.edu>
2020-03-26 20:53:51 +0200

    Continuous Learning
    Trained PPO2, SAC, TD3 agents continuously on highway scenario

--- rl_fyp/gym_rsu/script.py
+++ rl_fyp/gym_rsu/script.py
@@ -241,7 +241,7 @@ elif argumentList.__len__() >= 5:
                 #                            v=int(params_dict["v"]))
 
                 print(f'Loading {str(argumentList[agent_index])} agent with cars={params_dict["cars"]}')
-                model_online = TD3.load(f'rsu_agents/{traffic_scenario_name}_agents/continuous_learning_traffic_light/'
+                model_online = PPO2.load(f'rsu_agents/{traffic_scenario_name}_agents/continuous_learning_traffic_light/'
                                         f'{str(argumentList[agent_index])}_cl/{str(argumentList[agent_index])}'
                                         f'_ns3_{traffic_scenario_name}_cars={params_dict["cars"]}_CL')
 
